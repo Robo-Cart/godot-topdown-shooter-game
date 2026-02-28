d40b72ff5acd5b036b66279415fecbd90b28211f
David C <d.p.cartwright@gmail.com>
2026-02-28 12:56:11 +0000

    Turret fixes

---
+++ godot/root/scenes/scene/game_scene/game_content/game_entities/enemy_turrets/turret.gd
@@ -0,0 +1,56 @@
+class_name Turret
+extends CharacterBody2D
+
+@export var animation_tree: AnimationTree
+
+var direction: Vector2
+var playback: AnimationNodeStateMachinePlayback
+var health
+var dead: bool = false
+
+@onready var player: Player
+@onready var healthbar = $HealthBar
+
+
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+	add_to_group("enemy")
+	playback = animation_tree["parameters/playback"]
+	playback.travel("Idle")
+
+	health = 5
+	healthbar.init_health(health)
+
+
+func _physics_process(delta: float) -> void:
+	direction = (player.position - position).normalized()
+	#velocity = direction * speed
+
+	move_and_slide()
+	update_animation_parameters()
+
+
+func update_animation_parameters():
+	animation_tree["parameters/Idle/blend_position"] = direction
+
+
+func _set_health(value):
+	if !dead:
+		if health > 0:
+			healthbar.health = health
+		else:
+			dead = true
+			$DieParticle.emitting = true
+			set_deferred("monitoring", false)
+			get_node("Sprite2D").hide()
+			$Explode.play()
+			$DieTime.start()
+
+
+func _take_damage(value):
+	health += value
+	_set_health(health)
+
+
+func _on_die_time_timeout() -> void:
+	queue_free()
--- godot/root/scenes/scene/game_scene/game_content/game_entities/object_spawner/spawner.gd
+++ godot/root/scenes/scene/game_scene/game_content/game_entities/object_spawner/spawner.gd
@@ -7,12 +7,15 @@ extends StaticBody2D
 @export var spawn_location_variation_x: float = 10
 @export var spawn_location_variation_y: float = 30
 
-@export var enemy_scene = preload("res://Scenes/enemy.tscn")
+@export var enemy_scene = preload(
+	"res://root/scenes/scene/game_scene/game_content/game_entities/enemy_slime/enemy_slime.tscn"
+)
 
 @onready var spawn_area_location = $SpawnAreaMarker
 
-var spawn_timer : Timer
+var spawn_timer: Timer
 var number_of_spawned_enemies: int = 0
+
 
 func _ready():
 	spawn_timer = Timer.new()
@@ -21,17 +24,25 @@ func _ready():
 	spawn_timer.autostart = true
 	add_child(spawn_timer)
 
+
 func _process(delta: float) -> void:
-	if (!active):
+	if !active:
 		if spawn_timer.paused == false:
 			spawn_timer.paused = true
 	else:
 		if spawn_timer.paused == true:
 			spawn_timer.paused = false
 
+
 func on_timer_finished():
 	var spawned_enemy = enemy_scene.instantiate()
 	spawned_enemy.add_to_group("enemies")
 	get_tree().root.add_child(spawned_enemy)
-	spawned_enemy.global_position.x = spawn_area_location.global_position.x + randf_range(-spawn_location_variation_x, spawn_location_variation_x)
-	spawned_enemy.global_position.y = spawn_area_location.global_position.y + randf_range(-spawn_location_variation_y, spawn_location_variation_y)
+	spawned_enemy.global_position.x = (
+		spawn_area_location.global_position.x
+		+ randf_range(-spawn_location_variation_x, spawn_location_variation_x)
+	)
+	spawned_enemy.global_position.y = (
+		spawn_area_location.global_position.y
+		+ randf_range(-spawn_location_variation_y, spawn_location_variation_y)
+	)
