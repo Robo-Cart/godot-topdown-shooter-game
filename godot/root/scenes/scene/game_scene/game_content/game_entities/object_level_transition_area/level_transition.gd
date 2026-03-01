12523a08ec7a6132e8f568e977740feeb93b8aee
David C <d.p.cartwright@gmail.com>
2026-03-01 12:42:42 +0000

    Import of partially functional level transition code, ready to be completed

---
+++ godot/root/scenes/scene/game_scene/game_content/game_entities/object_level_transition_area/level_transition.gd
@@ -0,0 +1,72 @@
+@tool
+class_name LevelTransition extends Area2D
+
+enum SIDE { LEFT, RIGHT, TOP, BOTTOM }
+
+@export_file("*.tscn") var level: String
+@export var target_transition_area: String = "LevelTransition"
+
+@export_category("Collision Area Settings")
+
+@export_range(1, 12, 1, "or_greater") var size: int = 2:
+	set(_v):
+		size = _v
+		_update_area()
+
+@export var pixel_size: int = 32
+@export var side: SIDE = SIDE.LEFT:
+	set(_v):
+		side = _v
+		_update_area()
+
+@export var snap_to_grid: bool = false:
+	set(_v):
+		_snap_to_grid()
+
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+
+
+func _ready() -> void:
+	_update_area()
+	if Engine.is_editor_hint():
+		return
+
+	# monitoring = false
+
+	body_entered.connect(_player_entered)
+
+	pass
+
+
+func _player_entered(_player: Node2D) -> void:
+	# call level manager
+	pass
+
+
+func _update_area() -> void:
+	var new_rect: Vector2 = Vector2(pixel_size, pixel_size)
+	var new_position: Vector2 = Vector2.ZERO
+
+	if side == SIDE.TOP:
+		new_rect.x *= size
+		new_position.y -= pixel_size / 2
+	elif side == SIDE.BOTTOM:
+		new_rect.x *= size
+		new_position.y += pixel_size / 2
+	elif side == SIDE.LEFT:
+		new_rect.y *= size
+		new_position.x -= pixel_size / 2
+	elif side == SIDE.RIGHT:
+		new_rect.y *= size
+		new_position.x += pixel_size / 2
+
+	if collision_shape == null:
+		collision_shape = get_node("CollisionShape2D")
+
+	collision_shape.shape.size = new_rect
+	collision_shape.position = new_position
+
+
+func _snap_to_grid() -> void:
+	position.x = round(position.x / (pixel_size / 2)) * 16
+	position.y = round(position.y / (pixel_size / 2)) * 16
