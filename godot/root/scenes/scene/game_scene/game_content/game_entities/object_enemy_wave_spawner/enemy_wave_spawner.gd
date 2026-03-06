166f13e404928a79c8238aebe766cec1e724f935
David C <d.p.cartwright@gmail.com>
2026-03-06 08:19:17 +0000

    Started work on wave spawner

--- godot/root/scenes/scene/game_scene/game_content/game_entities/object_enemy_wave_spawner/enemy_wave_spawner.gd
+++ godot/root/scenes/scene/game_scene/game_content/game_entities/object_enemy_wave_spawner/enemy_wave_spawner.gd
@@ -1,4 +1,3 @@
-class_name Spawner
 extends StaticBody2D
 
 @export_group("Spawner Parameters")
@@ -7,17 +6,17 @@ extends StaticBody2D
 @export var spawn_location_variation_x: float = 10
 @export var spawn_location_variation_y: float = 30
 
-@export var enemy_scene = preload(
+@export var enemy_scene: Resource = preload(
 	"res://root/scenes/scene/game_scene/game_content/game_entities/enemy_slime/enemy_slime.tscn"
 )
 
-@onready var spawn_area_location = $SpawnAreaMarker
+@onready var spawn_area_location: Node2D = $SpawnAreaMarker
 
 var spawn_timer: Timer
 var number_of_spawned_enemies: int = 0
 
 
-func _ready():
+func _ready() -> void:
 	spawn_timer = Timer.new()
 	spawn_timer.wait_time = 1 / spawns_per_second
 	spawn_timer.timeout.connect(on_timer_finished)
@@ -25,7 +24,7 @@ func _ready():
 	add_child(spawn_timer)
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if !active:
 		if spawn_timer.paused == false:
 			spawn_timer.paused = true
@@ -34,8 +33,8 @@ func _process(delta: float) -> void:
 			spawn_timer.paused = false
 
 
-func on_timer_finished():
-	var spawned_enemy = enemy_scene.instantiate()
+func on_timer_finished() -> void:
+	var spawned_enemy: Enemy = enemy_scene.instantiate()
 	spawned_enemy.add_to_group("enemies")
 	get_tree().root.add_child(spawned_enemy)
 	spawned_enemy.global_position.x = (
