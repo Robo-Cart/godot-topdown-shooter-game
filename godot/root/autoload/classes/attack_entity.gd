8dea3a03f2b79fe6aa38614424f5fd7157dbf216
David C <d.p.cartwright@gmail.com>
2026-03-08 10:08:26 +0000

    Last bit of cleanup before break

--- godot/root/autoload/classes/attack_entity.gd
+++ godot/root/autoload/classes/attack_entity.gd
@@ -3,4 +3,4 @@ class_name AttackEntity
 # Strictly a data class, used for passing attack information
 # between hurtboxes and hitboxes
 
-var damage := 10.0
+var damage: float = 10.0
--- godot/root/scenes/scene/game_scene/game_content/game_entities/object_enemy_wave_spawner/enemy_wave_spawner.gd
+++ godot/root/scenes/scene/game_scene/game_content/game_entities/object_enemy_wave_spawner/enemy_wave_spawner.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 enum Side { NORTH, EAST, SOUTH, WEST }
 
-@export_file() var enemy_spawn_config: String
 @export var side: Side
 
 
