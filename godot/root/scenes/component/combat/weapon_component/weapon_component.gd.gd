2d427365263072f5f3b00cc59c4bd31cd9c5d6fd
David C <d.p.cartwright@gmail.com>
2026-03-10 19:04:52 +0000

    Refactored player to use components

---
+++ godot/root/scenes/component/combat/weapon_component/weapon_component.gd.gd
@@ -0,0 +1,52 @@
+extends Node2D
+class_name WeaponComponent
+
+signal weapon_fired(recoil_force: float, look_vector: Vector2)
+
+@export_group("Nodes")
+@export var bullet_scene: PackedScene
+@export var shoot_pos: Marker2D
+@export var gun_shot_audio: AudioStreamPlayer2D
+@export var muzzle_particles: CPUParticles2D
+
+@export_group("Stats")
+@export var bullet_speed: float = 1000.0
+@export var time_between_shots: float = 0.25
+@export var shot_force: float = 50.0  # The recoil pushback
+
+var can_shoot: bool = true
+var timer: Timer
+
+
+func _ready() -> void:
+	timer = Timer.new()
+	timer.wait_time = time_between_shots
+	timer.one_shot = true
+	timer.timeout.connect(_on_timer_timeout)
+	add_child(timer)
+
+
+func fire(look_vector: Vector2) -> void:
+	if not can_shoot or not bullet_scene or not shoot_pos:
+		return
+
+	can_shoot = false
+	timer.start()
+
+	if gun_shot_audio:
+		gun_shot_audio.play()
+	if muzzle_particles:
+		muzzle_particles.emitting = true
+
+	var new_bullet: Node2D = bullet_scene.instantiate()
+	new_bullet.global_position = shoot_pos.global_position
+	new_bullet.global_rotation = shoot_pos.global_rotation
+	new_bullet.speed = bullet_speed
+
+	get_tree().current_scene.add_child(new_bullet)
+
+	weapon_fired.emit(shot_force, look_vector)
+
+
+func _on_timer_timeout() -> void:
+	can_shoot = true
--- godot/root/scenes/scene/game_scene/game_content/game_entities/player/scripts/player.gd
+++ godot/root/scenes/scene/game_scene/game_content/game_entities/player/scripts/player.gd
@@ -1,5 +1,4 @@
 class_name Player
-
 extends CharacterBody2D
 
 @export_group("Player Parameters")
@@ -8,62 +7,42 @@ extends CharacterBody2D
 var animation_tree: AnimationTree = $SubViewportContainer/SubViewport/Player_Man_3D.get_node(
 	"AnimationTree"
 )
-@onready var weapon_path: Array[Node] = (
-	$SubViewportContainer/SubViewport/Player_Man_3D/Armature/Skeleton3D/BoneAttachment3D
-	. get_children()
-)
-@onready var ShootPos: Marker2D = $CentrePoint/ShootPos
-@onready var GunShot: AudioStreamPlayer2D = $GunShot
-#@onready var Impact: AudioStreamPlayer2D = $Impact
-@onready var MuzzleParticles: CPUParticles2D = $CentrePoint/ShootPos/MuzzleParticles
 @onready var Camera: Camera2D = $Camera2D
+
+@onready var weapon_comp: WeaponComponent = $WeaponComponent
 
 @export var speed: float = 200
 @export var physicscontrol: bool = false
-@export var bullet_speed: int = 1000
 @export var MAX_SPEED: float = 200.0
 @export var ACCELERATION: float = 800.0
 
 var permanent_buffs: Dictionary = {}
-
 const FRICTION = 900.0
-
-const IS_PLAYER = true
 
 var input_move: Vector2
 var input_aim: Vector2
 var playback: AnimationNodeStateMachinePlayback
-var anim_pos: Vector2
 var look_vector: Vector2
 var player_offset_angle: float = 89.5
-
-var bullet_scene: PackedScene = preload(
-	"res://root/scenes/scene/game_scene/game_content/game_entities/object_bullet/bullet01.tscn"
-)
-var time_between_shot: float = 0.25
-var can_shoot: bool = true
-var shot_force: float = 50
-
 var mouse_captured: bool = false
 
 
 func _ready() -> void:
 	add_to_group("player")
 	playback = animation_tree["parameters/playback"]
-	$ShootTimer.wait_time = time_between_shot
+
+	if weapon_comp:
+		weapon_comp.weapon_fired.connect(_on_weapon_fired)
 
 
 func _physics_process(delta: float) -> void:
 	input_move = Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
 	input_aim = Input.get_vector("look_left", "look_right", "look_up", "look_down")
 
-	# Check player control to use physics based control
 	if physicscontrol:
 		if input_move:
-			# Acceleration (Weighty start)
 			velocity = velocity.move_toward(input_move * MAX_SPEED, ACCELERATION * delta)
 		else:
-			# Friction (Weighty stop)
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	else:
 		velocity = input_move * speed
@@ -72,47 +51,30 @@ func _physics_process(delta: float) -> void:
 		player_man.rotation.y = -input_aim.angle() + player_offset_angle
 		$CentrePoint.global_rotation = input_aim.angle()
 		look_vector = input_aim.normalized()
-	if input_move != Vector2.ZERO and input_aim == Vector2.ZERO:
+	elif input_move != Vector2.ZERO:  # (Optimized your previous if/else block)
 		player_man.rotation.y = -input_move.angle() + player_offset_angle
 		$CentrePoint.global_rotation = input_move.angle()
 		look_vector = input_move.normalized()
 
-	if Input.is_action_pressed("fire") and can_shoot:
-		_shoot()
-		can_shoot = false
-		$ShootTimer.start()
-		GunShot.play()
-		MuzzleParticles.emitting = true
-		#shake camera
-		$Camera2D.add_trauma(0.15)
-		#apply force to player from front shot_force
-		velocity -= look_vector * shot_force
+	if Input.is_action_pressed("fire"):
+		weapon_comp.fire(look_vector)
 
 	move_and_slide()
 	select_animation()
 	update_animation_parameters()
 
 
+func _on_weapon_fired(recoil: float, l_vector: Vector2) -> void:
+	Camera.add_trauma(0.15)
+	velocity -= l_vector * recoil
+
+
 func add_buff(buff_name: String) -> void:
-	# If we already have this buff, add 1 to the stack. Otherwise, start at 1.
 	if permanent_buffs.has(buff_name):
 		permanent_buffs[buff_name] += 1
 	else:
 		permanent_buffs[buff_name] = 1
-
 	LogWrapper.debug(self, "Player Buffs Updated: " + str(permanent_buffs))
-
-
-func _shoot() -> void:
-	var new_bullet: Node = bullet_scene.instantiate()
-	new_bullet.global_position = ShootPos.global_position
-	new_bullet.global_rotation = ShootPos.global_rotation
-	new_bullet.speed = bullet_speed
-	get_parent().add_child(new_bullet)
-
-
-func _on_shot_timer_timeout() -> void:
-	can_shoot = true
 
 
 func select_animation() -> void:
@@ -123,15 +85,12 @@ func select_animation() -> void:
 
 
 func update_animation_parameters() -> void:
-	if input_aim != Vector2.ZERO and input_move == Vector2.ZERO:
-		input_move = Vector2(0, 0)
-	if input_move != Vector2.ZERO and input_aim == Vector2.ZERO:
-		input_move = input_move.rotated(player_man.rotation.y)
-	if input_move != Vector2.ZERO and input_aim != Vector2.ZERO:
-		input_move = input_move.rotated(player_man.rotation.y)
+	var blend_vec = input_move
+	if input_move != Vector2.ZERO:
+		blend_vec = input_move.rotated(player_man.rotation.y)
 
-	animation_tree["parameters/Run/blend_position"] = input_move
-	animation_tree["parameters/Walk/blend_position"] = input_move
+	animation_tree["parameters/Run/blend_position"] = blend_vec
+	animation_tree["parameters/Walk/blend_position"] = blend_vec
 
 
 func capture_mouse() -> void:
