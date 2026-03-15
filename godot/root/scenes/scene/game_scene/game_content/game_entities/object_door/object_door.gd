b621bb0976853d27df4520f85a305cdd326b0992
David C <d.p.cartwright@gmail.com>
2026-03-15 19:05:12 +0000

    fix door visual switching

--- godot/root/scenes/scene/game_scene/game_content/game_entities/object_door/object_door.gd
+++ godot/root/scenes/scene/game_scene/game_content/game_entities/object_door/object_door.gd
@@ -1,22 +1,65 @@
+@tool
 class_name ObjectDoor
 extends Node2D
 
-var is_open: bool = false
+enum ORIENTATION {
+	HORIZONTAL,
+	VERTICAL
+}
 
-@onready var anim_player: AnimationPlayer = $AnimationPlayer
+@export var orientation_setting: ORIENTATION = ORIENTATION.HORIZONTAL:
+	set(v):
+		orientation_setting = v
+		if is_inside_tree():
+			_update_visuals()
+
+
+var is_open: bool = false
+var anim_player: AnimationPlayer
+
 
 func _ready() -> void:
-	open_door()
+	_update_visuals()
+	if not Engine.is_editor_hint():
+		open_door()
+
+
+func _update_visuals() -> void:
+	var h_sprite: Sprite2D = find_child("HorizontalSprite2D", true, false)
+	var v_sprite1: Sprite2D = find_child("VerticalSprite2D1", true, false)
+	var v_sprite2: Sprite2D = find_child("VerticalSprite2D2", true, false)
+
+	if orientation_setting == ORIENTATION.HORIZONTAL:
+		if h_sprite:
+			h_sprite.visible = true
+		if v_sprite1:
+			v_sprite1.visible = false
+		if v_sprite2:
+			v_sprite2.visible = false
+		anim_player = find_child("HorizontalAnimationPlayer", true, false)
+		LogWrapper.debug(self, "Horizontal door selected.")
+	else:
+		if h_sprite:
+			h_sprite.visible = false
+		if v_sprite1:
+			v_sprite1.visible = true
+		if v_sprite2:
+			v_sprite2.visible = true
+		anim_player = find_child("VerticalAnimationPlayer", true, false)
+		LogWrapper.debug(self, "Vertical door selected.")
+
 
 func open_door() -> void:
 	if is_open:
 		return
-	anim_player.play("door_open")
+	if anim_player:
+		anim_player.play("open_door")
 	is_open = true
+
 
 func close_door() -> void:
 	if !is_open:
 		return
-
-	anim_player.play("door_close")
+	if anim_player:
+		anim_player.play("close_door")
 	is_open = false
