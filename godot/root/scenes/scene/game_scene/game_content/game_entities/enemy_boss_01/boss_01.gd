df406fa157416c8285f84db4c1f4e2b621113c1b
David C <d.p.cartwright@gmail.com>
2026-03-15 13:56:56 +0000

    Final boss updates for now

---
+++ godot/root/scenes/scene/game_scene/game_content/game_entities/enemy_boss_01/boss_01.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@onready var health_component: HealthComponent = $HealthComponent
+@onready var hit_flash_anim: AnimationPlayer = $HitFlashAnim
+
+
+func _ready() -> void:
+	add_to_group("enemy")
+
+	if health_component:
+		health_component.damaged.connect(_on_damaged)
+
+
+func _on_damaged(_attack: AttackEntity) -> void:
+	if hit_flash_anim:
+		hit_flash_anim.play("hit")
