e4eefbc6958fe7a5fe14a7c904d9a70fc6ee1947
David C <d.p.cartwright@gmail.com>
2026-03-15 11:42:48 +0000

    updated areashields to be separate scenes

--- godot/root/scenes/scene/game_scene/game_content/game_entities/enemy_boss_01/boss_01_area_shield.gd
+++ godot/root/scenes/scene/game_scene/game_content/game_entities/enemy_boss_01/boss_01_area_shield.gd
@@ -1,30 +1,16 @@
 extends Node2D
 
-var health: int
-var dead: bool = false
+@onready var health_component: HealthComponent = $ShieldBody/HealthComponent
+@onready var hit_flash_anim: AnimationPlayer = $ShieldBody/HitFlashAnim
 
 
 func _ready() -> void:
 	add_to_group("enemy")
-	health = 10
+
+	if health_component:
+		health_component.damaged.connect(_on_damaged)
 
 
-func _set_health(_value: int) -> void:
-	if !dead:
-		if health <= 0:
-			dead = true
-			$DieParticle.emitting = true
-			set_deferred("monitoring", false)
-			get_node("Sprite2D").hide()
-			$DieTime.start()
-
-
-func _take_damage(value: int) -> void:
-	health += value
-	_set_health(health)
-	$HitFlashAnim.play("hit")
-
-
-func _on_die_time_timeout() -> void:
-	self.hide()
-	queue_free()
+func _on_damaged(_attack: AttackEntity) -> void:
+	if hit_flash_anim:
+		hit_flash_anim.play("hit")
